fab467d4249eb170c756644f06ba6fb3b4c6fe74
ArMANIAK <reklama.alatyr@gmail.com>
2020-08-15 23:15:02 +0300

    added home-controller method

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,9 +5,20 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$data['server_answer'] = 'smth';
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		$this->load->view('feed');
+		$this->load->view('feed', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function getNewsByDate()
+	{
+		$this->load->model('posts_model');
+		$data['server_answer'] = $this->posts_model->test();
+		$this->load->view('templates/header');
+		$this->load->view('templates/navbar');
+		$this->load->view('feed', $data);
 		$this->load->view('templates/footer');
 	}
 }
