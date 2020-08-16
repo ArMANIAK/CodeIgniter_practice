e6d906f214852082fc7b8e911c7a4fa4ac8987cc
ArMANIAK <reklama.alatyr@gmail.com>
2020-08-16 23:49:21 +0300

    added news class

---
+++ application/libraries/News.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class News {
+
+    public $id;
+    public $title;
+    public $body;
+    public $preview;
+    public $image;
+    public $author;
+    public $date;
+    public $tags;
+}
