283af3b1359a39f6ccceef3f8600ab778344b637
ArMANIAK <reklama.alatyr@gmail.com>
2020-08-17 22:58:19 +0300

    edited author class

--- application/libraries/Author.php
+++ application/libraries/Author.php
@@ -1,11 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class News {
+class Author {
 
     public $id;
     public $name;
     public $avatar;
     public $socialNetworks;
 
+    public function __construct($id, $name, $avatar, $socialNetworks)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->avatar = $avatar;
+        $this->socialNetworks = $socialNetworks;
+    }
 }
