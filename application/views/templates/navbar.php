808497b402acdb3ee9b71bcb01df05036d27b654
ArMANIAK <reklama.alatyr@gmail.com>
2020-08-17 23:17:48 +0300

    edited testing link

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li>Home page</li>
-        <li><a href="<?= base_url('index.php/home/getNewsByDate'); ?>">Latest news</a></li>
+        <li><a href="<?= base_url('index.php/home/getPostsByDate/' . date('y-m-d')); ?>">Latest news</a></li>
         <li>Most popular</li>
         <li>Our bloggers</li>
         <li>In case of emergency</li>
