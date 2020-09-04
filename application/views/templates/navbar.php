3c07167146b6b3e579f0ae812bce2a465b6031c6
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-04 22:59:48 +0300

    editted links in navbar

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-        <li>Home page</li>
-        <li><a href="<?= base_url('index.php/home/getPostsByDate/' . date('y-m-d')); ?>">Latest news</a></li>
-        <li>Most popular</li>
-        <li>Our bloggers</li>
+        <li><a href="<?= base_url(); ?>">Home page</a></li>
+        <li><a href="<?= base_url('index.php/posts/getPostsByDate/' . date('yy-m-d')); ?>">Today's news</a></li>
+        <li><a href="<?= base_url('index.php/posts/getPostsByDate'); ?>">Most popular</a></li>
+        <li><a href="<?= base_url('index.php/posts/getAuthorsList'); ?>">Our bloggers</a></li>
         <li>In case of emergency</li>
     </ul>
 </nav>
