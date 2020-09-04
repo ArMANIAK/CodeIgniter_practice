894cb46837e89e0347f425364b50171c42b37098
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-04 22:51:52 +0300

    add filterByDate method to posts_model

--- application/models/Posts_model.php
+++ application/models/Posts_model.php
@@ -19,4 +19,13 @@ class Posts_model extends CI_Model
         return $result->result();
     }
 
+    public function filterByDate($date) {
+        $sql = "SELECT *
+                FROM posts
+                RIGHT JOIN authors 
+                ON posts.author = authors.id
+                WHERE posts.date='$date'";
+        $result = $this->db->query($sql);
+        return $result->result();
+    }
 }
