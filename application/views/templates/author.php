9157f3d5afa6c7c91ad0b79a33d4eb5b79eceee6
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-06 00:24:52 +0300

    unclosed a-tag closed

--- application/views/templates/author.php
+++ application/views/templates/author.php
@@ -1,4 +1,4 @@
 <article class="author">
     <img class="avatar" src="<?=$author->avatar?>" alt="Avatar of <?=$author->name?>" />
-    <a href="<?= base_url("index.php/posts/getPostsByAuthor/$author->id"); ?>"><h3><?=$author->name?></h3>
+    <a href="<?= base_url("index.php/posts/getPostsByAuthor/$author->id"); ?>"><h3><?=$author->name?></h3></a>
 </article>
