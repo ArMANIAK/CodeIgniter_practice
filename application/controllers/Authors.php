05aba508d04fadb56c822f04b38009ce32da6314
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-06 00:25:37 +0300

    corrected model name call

--- application/controllers/Authors.php
+++ application/controllers/Authors.php
@@ -5,10 +5,10 @@ class Authors extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->model('posts_model');
+		$this->load->model('authors_model');
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		foreach($this->posts_model->authorsList() as $data['author']) $this->load->view('templates/author', $data);
+		foreach($this->authors_model->authorsList() as $data['author']) $this->load->view('templates/author', $data);
 		$this->load->view('templates/footer');
 	}
 }
