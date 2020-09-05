507da0c3d20ec5b2af3b8dd85d2ed884896f5dd6
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-06 00:10:15 +0300

    added calendar to navbar

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,9 +1,11 @@
 <nav>
+    <?= $this->calendar->generate();?>
     <ul>
         <li><a href="<?= base_url(); ?>">Home page</a></li>
         <li><a href="<?= base_url('index.php/posts/getPostsByDate/' . date('yy-m-d')); ?>">Today's news</a></li>
         <li><a href="<?= base_url('index.php/posts/getPostsByDate'); ?>">Most popular</a></li>
         <li><a href="<?= base_url('index.php/posts/getAuthorsList'); ?>">Our bloggers</a></li>
         <li>In case of emergency</li>
+        <li>really long string In case of emergency really long string In case of emergency really long string In case of emergency</li>
     </ul>
 </nav>
