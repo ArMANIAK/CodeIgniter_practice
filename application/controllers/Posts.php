d8548992d2f9f229d17f3e76c6703bfb8285d50d
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-08 23:28:02 +0300

    added tags to render ews

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -8,7 +8,11 @@ class Posts extends CI_Controller {
 		$this->load->model('posts_model');
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		foreach($this->posts_model->getAllPosts() as $data['post']) $this->load->view('templates/post', $data);
+		foreach($this->posts_model->getAllPosts() as $data['post']) 
+		{
+			$data['tags'] = $this->posts_model->getTagsForPost($data['post']->id);
+			$this->load->view('templates/post', $data);
+		}
 		$this->load->view('templates/footer');
 	}
 
@@ -17,7 +21,11 @@ class Posts extends CI_Controller {
 		$this->load->model('posts_model');
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		foreach($this->posts_model->filterByDate($date) as $data['post']) $this->load->view('templates/post', $data);
+		foreach($this->posts_model->filterByDate($date) as $data['post']) 
+		{
+			$data['tags'] = $this->posts_model->getTagsForPost($data['post']->id);
+			$this->load->view('templates/post', $data);
+		}
 		$this->load->view('templates/footer');
 	}
 
@@ -26,7 +34,11 @@ class Posts extends CI_Controller {
 		$this->load->model('posts_model');
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		foreach($this->posts_model->filterByAuthor($authorId) as $data['post']) $this->load->view('templates/post', $data);
+		foreach($this->posts_model->filterByAuthor($authorId) as $data['post']) 
+		{
+			$data['tags'] = $this->posts_model->getTagsForPost($data['post']->id);
+			$this->load->view('templates/post', $data);
+		}
 		$this->load->view('templates/footer');
 	}
 }
--- application/views/templates/post.php
+++ application/views/templates/post.php
@@ -1,10 +1,18 @@
 <article class="post">
-    <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
+    <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
     <main class="post">
         <h3 class="post-title"><?=$post->title?></h3>
-        <blockquote>Written by <?=$post->name?></blockquote>
+        <blockquote>Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a></blockquote>
+        <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
         <p><?=$post->body?></p>
     </main>
-    <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
+    <p>
+        <?php foreach ($tags as $tag):?>
+            <span> <?=$tag->tag?> </span>
+        <?php endforeach ?>
+    </p>
     <p><?=$post->date?></p>
+
+    <!-- Add tags here -->
+
 </article>
