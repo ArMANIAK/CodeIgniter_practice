78355036f6ddad05a0cbf99261ffcd9c584bf5db
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-12 23:23:01 +0300

    edited post structure

--- application/views/templates/post.php
+++ application/views/templates/post.php
@@ -2,17 +2,16 @@
     <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
     <main class="post">
         <h3 class="post-title"><?=$post->title?></h3>
-        <blockquote>Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a></blockquote>
-        <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
-        <p><?=$post->body?></p>
+        <p>
+            Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a>
+            <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
+        </p>
+        <p><?=$post->preview?></p>
+        <p>
+            <?php foreach ($tags as $tag):?>
+                <a href="<?=base_url("index.php/posts/getPostsByTag/$tag->tag_id")?>"> <?=$tag->tag?> </a>
+            <?php endforeach ?>
+        </p>
+        <p><a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a></p>
     </main>
-    <p>
-        <?php foreach ($tags as $tag):?>
-            <span> <?=$tag->tag?> </span>
-        <?php endforeach ?>
-    </p>
-    <p><?=$post->date?></p>
-
-    <!-- Add tags here -->
-
 </article>
