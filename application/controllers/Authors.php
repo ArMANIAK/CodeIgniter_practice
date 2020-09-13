458a09408f765420c29e2ecb8a73b94e6507707c
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-13 22:49:27 +0300

    slightly improved index method authors-controller

--- application/controllers/Authors.php
+++ application/controllers/Authors.php
@@ -8,7 +8,11 @@ class Authors extends CI_Controller {
 		$this->load->model('authors_model');
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
-		foreach($this->authors_model->authorsList() as $data['author']) $this->load->view('templates/author', $data);
+		foreach($this->authors_model->authorsList() as $data['author']) 
+		{
+			$this->load->view('templates/author', $data);
+		}
 		$this->load->view('templates/footer');
 	}
+
 }
