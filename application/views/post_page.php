5d55ef0750af57a5e61bb5681431fc2458eea808
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-19 23:36:25 +0300

    added post_page

---
+++ application/views/post_page.php
@@ -0,0 +1,19 @@
+<article class="news">
+    <h3 class="news-title"><?=$post->title?></h3>
+    <p>
+        Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a>
+        <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
+    </p>
+    <main class="news-body">
+        <img class="newsPic" src="<?=$post->image?>" alt="<?=$post->preview?>" />
+        <p><?=$post->body?></p>
+        <p>
+            <?php foreach ($tags as $tag):?>
+                <a class="tag" href="<?=base_url("index.php/posts/getPostsByTag/$tag->tag_id")?>"> <?=$tag->tag?> </a>
+            <?php endforeach ?>
+        </p>
+        <p><a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a></p>
+        <p class="counter"><?=$post->newsViews?></p>
+    </main>
+</article>
+<script src='<?=base_url('public/counter.js')?>'></script>
