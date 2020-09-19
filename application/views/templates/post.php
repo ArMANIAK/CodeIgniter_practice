8f5dd7d7d8e8027693e57484c8f0257211b01921
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-19 23:38:15 +0300

    edited post template

--- application/views/templates/post.php
+++ application/views/templates/post.php
@@ -1,7 +1,7 @@
 <article class="post">
     <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
     <main class="post">
-        <h3 class="post-title"><?=$post->title?></h3>
+        <h3 class="post-title"><a href="<?=base_url("index.php/posts/showPostPage/$post->id")?>"><?=$post->title?></a></h3>
         <p>
             Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a>
             <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
@@ -9,9 +9,10 @@
         <p><?=$post->preview?></p>
         <p>
             <?php foreach ($tags as $tag):?>
-                <a href="<?=base_url("index.php/posts/getPostsByTag/$tag->tag_id")?>"> <?=$tag->tag?> </a>
+                <a class="tag" href="<?=base_url("index.php/posts/getPostsByTag/$tag->tag_id")?>"> <?=$tag->tag?> </a>
             <?php endforeach ?>
         </p>
         <p><a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a></p>
+        <p class="counter"><?=$post->newsViews?></p>
     </main>
 </article>
