8d22e4e6462e94eebd603b9c2164fac7c784b984
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-20 23:37:00 +0300

    added some styling

--- application/views/templates/post.php
+++ application/views/templates/post.php
@@ -1,10 +1,10 @@
 <article class="post">
     <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
+    <p class="counter"><?='Views: ' . $post->newsViews?></p>
     <main class="post">
         <h3 class="post-title"><a href="<?=base_url("index.php/posts/showPostPage/$post->id")?>"><?=$post->title?></a></h3>
         <p>
             Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a>
-            <img class="avatar" src="<?=$post->avatar?>" alt="Avatar of <?=$post->avatar?>" />
         </p>
         <p><?=$post->preview?></p>
         <p>
@@ -13,6 +13,5 @@
             <?php endforeach ?>
         </p>
         <p><a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a></p>
-        <p class="counter"><?=$post->newsViews?></p>
     </main>
 </article>
