fac14076b9d78b37932f46abda87c2386476180e
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-23 23:59:43 +0300

    redesigned post template

--- application/views/templates/post.php
+++ application/views/templates/post.php
@@ -1,17 +1,20 @@
 <article class="post">
     <img src="<?=$post->image?>" alt="<?=$post->preview?>" />
     <p class="counter"><?='Views: ' . $post->newsViews?></p>
+    <p class="date">
+        <a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a>
+    </p>
     <main class="post">
-        <h3 class="post-title"><a href="<?=base_url("index.php/posts/showPostPage/$post->id")?>"><?=$post->title?></a></h3>
+        <h3 class="post-title"><?=$post->title?></h3>
         <p>
             Written by <a href="<?=base_url("index.php/authors/showAuthorPage/$post->author")?>"><?=$post->name?></a>
         </p>
         <p><?=$post->preview?></p>
+        <p><a href="<?=base_url("index.php/posts/showPostPage/$post->id")?>">Read more --></a></p>
         <p>
             <?php foreach ($tags as $tag):?>
                 <a class="tag" href="<?=base_url("index.php/posts/getPostsByTag/$tag->tag_id")?>"> <?=$tag->tag?> </a>
             <?php endforeach ?>
         </p>
-        <p><a href="<?=base_url("index.php/posts/getPostsByDate/$post->date")?>"><?=$post->date?></a></p>
     </main>
 </article>
