90aaac961d8dd69ea81eea1536f0aa68b67c7c71
ArMANIAK <reklama.alatyr@gmail.com>
2020-09-24 00:56:47 +0300

    added links to calendar

--- application/controllers/Authors.php
+++ application/controllers/Authors.php
@@ -5,9 +5,10 @@ class Authors extends CI_Controller {
 
 	public function index()
 	{
+		$data['links'] = generateLinksForCalendar();
 		$this->load->model('authors_model');
 		$this->load->view('templates/header');
-		$this->load->view('templates/navbar');
+		$this->load->view('templates/navbar', $data);
 		foreach($this->authors_model->authorsList() as $data['author']) 
 		{
 			$this->load->view('templates/author', $data);
@@ -17,11 +18,12 @@ class Authors extends CI_Controller {
 
 	public function showAuthorPage($authorId)
 	{
+		$data['links'] = generateLinksForCalendar();
 		$this->load->model('posts_model');
 		$this->load->model('authors_model');
 		$data['author'] = $this->authors_model->getAuthorInfo($authorId)[0];
 		$this->load->view('templates/header');
-		$this->load->view('templates/navbar');
+		$this->load->view('templates/navbar', $data);
 		$this->load->view('templates/author', $data);
 		foreach($this->posts_model->filterByAuthor($authorId) as $data['post']) 
 		{
